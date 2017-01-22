fa6ee28296cbca2b8f100cb6d52df48e465d253d
kalyanlinuxcloud <kalyanlinuxcloud@gmail.com>
2017-01-22 14:40:49 +0530

    Update Ass8.1.readwrite.sh

--- Ass8.1.readwrite.sh
+++ Ass8.1.readwrite.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
 for File in *
-
 do
-
-#if [ -f $File ] ; then
-#	if [ -r $File ] ; then
-#		if [ -w $File ] ; then
-#		echo $File
-#		fi
-#	fi
-#fi
-
-
-#	or          or           or       or
-
-
 if [ -f $File ] ; then
 	if [ -r $File -a -w $File ] ; then
 	echo $File
 	fi
 fi
-
-
 done
 
